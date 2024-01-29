d9f80942ee257e4f34091f5e677b7e52eae9fc56
Matt Knight <mattnite@proton.me>
2024-01-28 19:58:41 -0800

    add rest of executables

--- build.zig
+++ build.zig
@@ -86,4 +86,60 @@ pub fn build(b: *Build) void {
     st_flash.linkLibrary(stlink);
     st_flash.linkLibrary(libusb);
     b.installArtifact(st_flash);
+
+    const st_info = b.addExecutable(.{
+        .name = "st-info",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    st_info.addCSourceFiles(&.{
+        "src/st-info/info.c",
+    }, c_flags);
+    st_info.addIncludePath(.{ .path = "inc" });
+    st_info.addIncludePath(.{ .path = "src/stlink-lib" });
+    st_info.addConfigHeader(version);
+    st_info.linkLibrary(stlink);
+    st_info.linkLibrary(libusb);
+    b.installArtifact(st_info);
+
+    const st_util = b.addExecutable(.{
+        .name = "st-util",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    st_util.addCSourceFiles(&.{
+        "src/st-util/gdb-remote.c",
+        "src/st-util/gdb-server.c",
+        "src/st-util/semihosting.c",
+    }, c_flags);
+    switch (target.getOsTag()) {
+        .windows => {
+            st_util.addIncludePath(.{ .path = "src/win32" });
+        },
+        else => {},
+    }
+    st_util.addIncludePath(.{ .path = "inc" });
+    st_util.addIncludePath(.{ .path = "src/stlink-lib" });
+    st_util.addConfigHeader(version);
+    st_util.linkLibrary(stlink);
+    st_util.linkLibrary(libusb);
+    b.installArtifact(st_util);
+
+    const st_trace = b.addExecutable(.{
+        .name = "st-trace",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    st_trace.addCSourceFiles(&.{
+        "src/st-trace/trace.c",
+    }, c_flags);
+    st_trace.addIncludePath(.{ .path = "inc" });
+    st_trace.addIncludePath(.{ .path = "src/stlink-lib" });
+    st_trace.addConfigHeader(version);
+    st_trace.linkLibrary(stlink);
+    st_trace.linkLibrary(libusb);
+    b.installArtifact(st_trace);
 }
